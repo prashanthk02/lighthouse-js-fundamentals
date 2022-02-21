7314f13a7a3383c22ddb5ededae145fe32578199
prashanthk02 <prashanth.k369@gmail.com>
2022-02-21 12:04:02 -0500

    aarrays

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,13 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
-console.log("Kitchen stuff to pack:");
+console.log("Kitchen stuff to pack: using for loop");
 for(let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+console.log("Kitchen stuff to pack: using while loop");
+let k = 0;
+while(k < packingList.length) {
+  console.log(packingList[k]);
+  k++;
 }
